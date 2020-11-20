cf9275d5541ebec2090a946265289e00549f8d30
takayukiyunoki <takayukiy@uabjb.edu.bo>
2020-11-20 19:39:02 -0400

    Construct the principal coordinates of neighbour matrices (PCNM) of overall model for the model communities in the center of large system.

---
+++ PCNM.pos.large.center.R
@@ -0,0 +1,17 @@
+# For large system
+n.sites = 1600
+landscape <- data.frame(sites = 1:n.sites, expand.grid(x = 1:sqrt(n.sites), y = 1:sqrt(n.sites)))
+landscape.center <- landscape[landscape$x >= 16 & landscape$x <= 25 & landscape$y >= 16 & landscape$y <= 25, ]
+dist.landscape <- dist(landscape.center[, c("x", "y")])
+pcnm <- pcnm(dist.landscape)
+PCNM.overall <- PCNM(dist.landscape)
+length(which(pcnm$values>0))
+select <- which(PCNM.overall$Moran_I$Positive==TRUE)
+length(select)
+par(mfrow = c(3,2))
+some.pcnm <- c(1, 2, 5, 10, 20, 50)
+for(i in 1:length(some.pcnm)) {
+sr.value(landscape.center[, c("x", "y")], PCNM.overall$vectors[, some.pcnm[i]], method = "greylevel", csize = 0.35, sub = some.pcnm[i], csub=2)
+}
+PCNM.pos.center <- as.data.frame(PCNM.overall$vectors)[,select]
+write.csv(PCNM.pos.center, file = "PCNM.pos.large.center.csv")
